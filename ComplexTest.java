92dc725454d74b4b4bb6d4990c810af9011d2227
kanaksoni714 <45351308+kanaksoni714@users.noreply.github.com>
2018-12-17 10:27:46 +0530

    Update ComplexTest.java

--- ComplexTest.java
+++ ComplexTest.java
@@ -1,20 +1,23 @@
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 
 public class ComplexTest {
 
+	private ComplexNumber complexnumber1;
+	private ComplexNumber complexnumber2;
+	private ComplexNumber complexnumber3;
+	
 	@Test
-	public void test(){}
-	public static Complex add(Complex n1, Complex n2)
-    {
-        Complex temp = new Complex(0.0, 0.0);
-
-        temp.real = n1.real + n2.real;
-        temp.imag = n1.imag + n2.imag;
-
-        assertEquals(0,0);
-		return temp;
-    }
+	public void ComplexTest(){
+		complexnumber1=new ComplexNumber();
+		complexnumber1.set(100,22);
+		complexnumber1.display();
+		complexnumber2=new ComplexNumber();
+		complexnumber2.set(11,20);
+		complexnumber2.display();
+		complexnumber3=new ComplexNumber();
+		complexnumber3=complexnumber3.sumOfComplexNumber(complexnumber1,complexnumber2);
+		complexnumber3.display();
+	}
+	
 }
