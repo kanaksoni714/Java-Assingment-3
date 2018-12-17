cb150c9d741f426ea3292432f688f21d522c42cc
kanaksoni714 <45351308+kanaksoni714@users.noreply.github.com>
2018-12-17 10:25:39 +0530

    Update Complex.java

--- Complex.java
+++ Complex.java
@@ -1,30 +1,24 @@
-public class Complex {
 
-    double real;
-    double imag;
+public class ComplexNumber {
+public float real;
+public float imaginary;
 
-    public Complex(double real, double imag) {
-        this.real = real;
-        this.imag = imag;
-    }
 
-    public static void main(String[] args) {
-        Complex n1 = new Complex(12.3, 4.5),
-                n2 = new Complex(3.4, 75.0),
-                temp;
+public void set(float real, float imaginary){
+	this.real=real;
+	this.imaginary=imaginary;
 
-        temp = add(n1, n2);
-
-        System.out.printf("Sum = %.1f + %.1fi", temp.real, temp.imag);
-    }
-
-    public static Complex add(Complex n1, Complex n2)
-    {
-        Complex temp = new Complex(0.0, 0.0);
-
-        temp.real = n1.real + n2.real;
-        temp.imag = n1.imag + n2.imag;
-
-        return(temp);
-    }
+	}
+public void display(){
+	System.out.println("Complex Number is="+this.real+"+"+"i"+this.imaginary);
 }
+
+public ComplexNumber sumOfComplexNumber(ComplexNumber object1,ComplexNumber object2)
+{	this.real=object1.real+object2.real;
+	this.imaginary=object1.imaginary+object2.imaginary;
+    
+    return this;
+}
+
+
+	}
